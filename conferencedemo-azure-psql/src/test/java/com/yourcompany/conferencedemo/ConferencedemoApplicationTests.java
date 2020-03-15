625381cd8ed02fb435edf4acb9e9aa600124e625
thenning <tim@tailoredtechsolutions.com>
2020-03-15 15:18:59 -0700

    Initial Java API with PostgreSQL

---
+++ conferencedemo-azure-psql/src/test/java/com/yourcompany/conferencedemo/ConferencedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourcompany.conferencedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferencedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

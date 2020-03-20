ff671a54f994dde9499f62fa2816c78abcbd98be
$conferencedemo-90 <tim@tailoredtechsolutions.com>
2020-03-19 17:55:01 -0700

    Added the Session to Speaker query model and repository.

---
+++ conferencedemo/src/main/java/com/yourcompany/conferencedemo/controllers/SessionSpeakerController.java
@@ -0,0 +1,25 @@
+package com.yourcompany.conferencedemo.controllers;
+
+
+import com.yourcompany.conferencedemo.models.SessionSpeaker;
+import com.yourcompany.conferencedemo.repositories.SessionSpeakerRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/sessionSpeakers")
+@CrossOrigin(origins="http://localhost:4200")
+public class SessionSpeakerController {
+
+    private final SessionSpeakerRepository sessionSpeakerRepository;
+
+    public SessionSpeakerController(SessionSpeakerRepository sessionSpeakerRepository) {
+        this.sessionSpeakerRepository = sessionSpeakerRepository;
+    }
+
+    @GetMapping("/{eventId}")
+    public List<SessionSpeaker> getSessionSpeaker(@PathVariable("eventId") Long eventId) {
+        return this.sessionSpeakerRepository.findByEventId(eventId);
+    }
+}
